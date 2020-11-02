be204eaeb22b5a988aa8325e8cc4d952573b201a
Mamadou-cherif <barrymamadoucherif289@gmail.com>
2020-11-02 19:41:34 +0100

    this is my first spring boot application commited on git

---
+++ src/test/java/com/ensat/WebAppApplication.java
@@ -0,0 +1,17 @@
+package com.ensat;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	
+	public static void main(String[]arg) {
+		SpringApplication.run(WebAppApplication.class, arg);
+		
+	}
+
+}
